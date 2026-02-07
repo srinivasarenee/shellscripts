2c48e85b15868a99f9692d03b613bebcdb9be31a
srinivasarenee <srinivas.renee@gmail.com>
2026-02-07 22:43:59 +0530

    code commit

--- 12-installationlogs.sh
+++ 12-installationlogs.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 # run this in ec2 as it is not running in gitbashgi
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
 
 echo "User : $USERID"
 if [ $USERID -ne 0 ]; then
-    echo "Run the script with Root User."
+    echo "Run the script with Root User." | tee -a $LOGS_FILE
     exit 1    
 fi
+mkdir -p $LOGS_FOLDER
 VALIDATE()
 {
-if [ $1 -ne 0]; then
-    echo "$2 Failure"
+if [ $1 -ne 0 ]; then
+    echo "$2 Failure" | tee -a $LOGS_FILE
     exit 1
 else
-    echo "$2 Success"
+    echo "$2 Success" | tee -a $LOGS_FILE
 fi
 
 }
 echo "Instaling NGINX"
-dnf install nginx -y
+dnf install nginx -y &>> $LOGS_FILE
 
 VALIDATE $? "Installing Nginx"
 
 echo "Instaling MySQL"
-dnf install mysql -y
+dnf install mysql -y &>> $LOGS_FILE
 
 VALIDATE $? "Installing MySQL"
 
 echo "Instaling NodeJS"
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGS_FILE
 
 VALIDATE $? "Installing NodeJS"
 
