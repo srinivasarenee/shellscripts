7f0eab2ce4c8ef6a836401c3f74e63e77d30beae
srinivasarenee <srinivas.renee@gmail.com>
2026-02-07 13:09:38 +0530

    updated

--- 04-readvars.sh
+++ 04-readvars.sh
@@ -3,3 +3,7 @@
 echo "Please enter Your User Name:"
 read USER_NAME
 echo "Your entered input is $USER_NAME"
+
+echo "Please enter Your Password:"
+read -s PASSWORD
+echo "Your entered Password is $PASSWORD"
